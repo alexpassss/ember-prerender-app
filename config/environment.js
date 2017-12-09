44252b4bd34cab3a26ddf1361248735a1a34e677
alexpassss <alexpassss@gmail.com>
2017-12-09 19:22:07 +0200

    Changed location type

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ember-prerender-test',
     environment,
     rootURL: '/',
-    locationType: '#!',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
